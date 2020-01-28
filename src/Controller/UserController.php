b3ae16fa574d2daf21685c455f23d51662356552
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2020-01-28 17:54:14 +0100

    fix conflicts

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,20 +2,13 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Form\UserType;
-use App\Security\LoginFormAuthenticator;
-use App\Services\GetUserClub;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 class UserController extends AbstractController
 {
